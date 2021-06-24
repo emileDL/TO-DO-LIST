4325093b2e1e83e3eb5a45b00a8a4edbf887fe95
Emile Dumais-Laporte <emile_d_l@hotmail.fr>
2021-06-24 10:06:43 -0400

    weird bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ToDoList from "./components/ToDoList";
 
 function App() {
   const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState([])
+  const todoRef = useRef()
 
-const handleSubmit = () => {}
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const name = todoRef.current.value
+  if(name === "") return
+  // console.log(name)
+  setNewTodo(name)
+  console.log(newTodo)
+  todoRef.current.value = null
+
+}
 
   return (
     <>
 <ToDoList todos = {todos}/>  
 <input type="text"
-placeholder="add a todo" />
+placeholder="add a todo" 
+ref = {todoRef}
+/>
 <input type="submit" value="add" 
 onClick={handleSubmit}
 />
 <div>{todos.length} left to do</div>
+{/* <div>
+{newTodo.map((e) => {
+  return(
+    console.log(e)
+  )
+})}
+</div> */}
+
     </>
   );
 }
