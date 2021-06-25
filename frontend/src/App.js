c370770042f081d617810f38fdbaf03ab778d4d2
Emile Dumais-Laporte <emile_d_l@hotmail.fr>
2021-06-25 17:56:17 -0400

    still weird

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ToDoList from "./components/ToDoList";
+import uuidv4 from "uuid/dist"
+
+const LOCAL_STORAGE_KEY = "todoApp.todos"
 
 function App() {
   const [todos, setTodos] = useState([])
-  const [newTodo, setNewTodo] = useState([])
+  // const [newTodo, setNewTodo] = useState([])
   const todoRef = useRef()
+
+useEffect(() =>{
+const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY)
+  if (storedTodos) setTodos(storedTodos)
+}, [])
+
+useEffect(() => {
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+}, [todos])
+
 
 const handleSubmit = (e) => {
   e.preventDefault()
   const name = todoRef.current.value
   if(name === "") return
-  // console.log(name)
-  setNewTodo(name)
-  console.log(newTodo)
+  console.log(name)
+  // setNewTodo(name)
+  // console.log(newTodo)
+  setTodos(prevTodos => {
+    return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+  })
   todoRef.current.value = null
 
 }
